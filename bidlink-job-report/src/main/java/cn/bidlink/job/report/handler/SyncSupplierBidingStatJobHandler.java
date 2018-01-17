106248414a9a4a535aa12f059c9527d6fea6019a
jiacaisu <jiacaisu@ebnew.com>
2018-01-17 14:01:05 +0800

    完善代码

--- bidlink-job-report/src/main/java/cn/bidlink/job/report/handler/SyncSupplierBidingStatJobHandler.java
+++ bidlink-job-report/src/main/java/cn/bidlink/job/report/handler/SyncSupplierBidingStatJobHandler.java
@@ -1,5 +1,6 @@
 package cn.bidlink.job.report.handler;
 
+import cn.bidlink.job.common.utils.DBUtil;
 import cn.bidlink.job.common.utils.SyncTimeUtil;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.annotation.JobHander;
@@ -7,9 +8,12 @@ import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.Date;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 /**
  * @author <a href="mailto:jiacaisu@ebnew.com">jiacaisu</a>
@@ -22,7 +26,9 @@ import java.util.Date;
 public class SyncSupplierBidingStatJobHandler extends SyncJobHandler /*implements InitializingBean*/ {
 
     private Logger logger = LoggerFactory.getLogger(SyncSupplierBidingStatJobHandler.class);
-
+    private final String SUPPLIER_ID = "supplier_id";
+    private final String COMPANY_ID = "company_id";
+    private final String SUPPLIER_TYPE = "supplier_type";
     @Override
     protected String getTableName() {
         return "supplier_biding_stat";
@@ -38,7 +44,7 @@ public class SyncSupplierBidingStatJobHandler extends SyncJobHandler /*implement
         //盘内供应商
         syncSupplierInner();
         //盘外供应商
-        syncSupplierOuter();
+      //  syncSupplierOuter();
         // 记录同步时间
         updateSyncLastTime();
         logger.info("同步供应商招标中标统计结束");
@@ -56,160 +62,32 @@ public class SyncSupplierBidingStatJobHandler extends SyncJobHandler /*implement
         Date lastSyncTime = getLastSyncTime();
         logger.info("同步盘内供应商招标中标统计开始lastSyncTime: " + new DateTime(lastSyncTime).toString("yyyy-MM-dd HH:mm:ss"));
 
-        String countSql = "SELECT\n" +
-                "\tCOUNT(DISTINCT B.BIDER_ID)\n" +
-                "FROM\n" +
-                "\tBID B\n" +
-                "LEFT JOIN PROJ_INTER_PROJECT P ON P.ID = B.PROJECT_ID\n" +
-                "WHERE\n" +
-                "\tp.COMPANY_ID = b.COMPANY_ID\n" +
-                "AND B.IS_BID_SUCCESS = 1\n" +
-                "AND P.PROJECT_STATUS IN (9, 11, 12)\n" +
-                "AND B.BIDER_ID IN (\n" +
-                "\tSELECT\n" +
-                "\t\tsupplier_id\n" +
-                "\tFROM\n" +
-                "\t\tbsm_company_supplier\n" +
-                ")";
-
-
-        String querySql = "SELECT\n" +
-                "\tt.pid,\n" +
-                "\tt.PROJECT_NAME,\n" +
-                "\tt.SUPPLIER_NAME,\n" +
-                "\tt.SUPPLIER_ID,\n" +
-                "\tt.TOTAL_PRICE,\n" +
-                "\tt2.bidCount,\n" +
-                "\tt.bidOkCount,\n" +
-                "t2.COMPANY_ID,\n" +
-                "1 as supplier_type\n" +
-                "FROM\n" +
-                "\t(\n" +
-                "\t\tSELECT\n" +
-                "\t\t\tP.ID AS pid,\n" +
-                "\t\t\tP.PROJECT_NAME,\n" +
-                "\t\t\tB.BIDER_NAME AS SUPPLIER_NAME,\n" +
-                "\t\t\tB.BIDER_ID AS SUPPLIER_ID,\n" +
-                "\t\t\tSUM(B.BIDER_PRICE_UNE) AS TOTAL_PRICE,\n" +
-                "\t\t\tCOUNT(b.ID) AS bidOkCount\n" +
+        String countSql = "\t\tSELECT\n" +
+                "\t\tcount(1)\n" +
                 "\t\tFROM\n" +
                 "\t\t\tBID B\n" +
                 "\t\tLEFT JOIN PROJ_INTER_PROJECT P ON P.ID = B.PROJECT_ID\n" +
-                "\t\tAND p.COMPANY_ID = b.COMPANY_ID\n" +
-                "\t\tWHERE\n" +
-                "\t\t\tB.IS_BID_SUCCESS = 1\n" +
-                "\t\tAND B.IS_ABANDON = 0\n" +
-                "\t\tAND B.IS_WITHDRAWBID = 0\n" +
-                "\t\tAND P.PROJECT_STATUS IN (9, 11, 12)\n" +
-                "\t\tGROUP BY\n" +
-                "\t\t\tB.BIDER_ID\n" +
-                "\t) t\n" +
-                "LEFT JOIN (\n" +
-                "\tSELECT\n" +
-                "\t\tB.BIDER_ID AS pid,\n" +
-                "\t\tCOUNT(p.id) AS bidCount,\n" +
-                "\t\tP.CREATE_TIME,\n" +
-                "\t\tb.COMPANY_ID\n" +
-                "\tFROM\n" +
-                "\t\tBID B\n" +
-                "\tLEFT JOIN PROJ_INTER_PROJECT P ON P.ID = B.PROJECT_ID\n" +
-                "\tAND p.COMPANY_ID = b.COMPANY_ID\n" +
-                "\tGROUP BY\n" +
-                "\t\tB.BIDER_ID\n" +
-                ") t2 ON t.SUPPLIER_ID = t2.pid\n" +
-                "\n" +
-                "WHERE\n" +
-                "\tt.SUPPLIER_ID IN (\n" +
-                "\t\tSELECT\n" +
-                "\t\t\tsupplier_id\n" +
-                "\t\tFROM\n" +
-                "\t\t\tbsm_company_supplier\n" +
-                "\t)\n" +
-                "GROUP BY\n" +
-                "\tt.SUPPLIER_ID  ORDER BY t2.CREATE_TIME DESC " +
-                "\t LIMIT ?,? \n";
-        ArrayList<Object> params = new ArrayList<>();
-//        params.add(lastSyncTime);
-        sync(ycDataSource, countSql, querySql, params);
-    }
-
-    private void syncSupplierOuter() {
-        Date lastSyncTime = getLastSyncTime();
-        logger.info("同步盘外供应商招标中标统计开始lastSyncTime: " + new DateTime(lastSyncTime).toString("yyyy-MM-dd HH:mm:ss"));
-
-        String countSql = "SELECT\n" +
-                "\tCOUNT(DISTINCT B.BIDER_ID)\n" +
-                "FROM\n" +
-                "\tBID B\n" +
-                "LEFT JOIN PROJ_INTER_PROJECT P ON P.ID = B.PROJECT_ID\n" +
-                "AND p.COMPANY_ID = b.COMPANY_ID\n" +
-                "WHERE\n" +
-                "\tB.IS_BID_SUCCESS = 1\n" +
-                "AND P.PROJECT_STATUS IN (9, 11, 12)\n" +
-                "AND B.BIDER_ID IN (\n" +
-                "\tSELECT\n" +
-                "\t\tsupplier_id\n" +
-                "\tFROM\n" +
-                "\t\tbsm_company_supplier_apply\n" +
-                "\tWHERE\n" +
-                "\t\tsupplier_status != 4\n" +
-                ")";
+                "\t\tAND p.COMPANY_ID = b.COMPANY_ID ";
 
 
-        String querySql = "SELECT\n" +
-                "\tt.pid,\n" +
-                "\tt.PROJECT_NAME,\n" +
-                "\tt.SUPPLIER_NAME,\n" +
-                "\tt.SUPPLIER_ID,\n" +
-                "\tt.TOTAL_PRICE,\n" +
-                "\tt2.bidCount,\n" +
-                "\tt.bidOkCount,\n" +
-                "  t.COMPANY_ID,\n" +
-                "  2 as supplier_type\n" +
-                "FROM\n" +
-                "\t(\n" +
-                "\t\tSELECT\n" +
+        String querySql = "\tSELECT\n" +
                 "\t\t\tP.ID AS pid,\n" +
-                "      B.COMPANY_ID,\n" +
+                "\t\t\tB.COMPANY_ID,\n" +
                 "\t\t\tP.PROJECT_NAME,\n" +
                 "\t\t\tB.BIDER_NAME AS SUPPLIER_NAME,\n" +
                 "\t\t\tB.BIDER_ID AS SUPPLIER_ID,\n" +
-                "\t\t\tSUM(B.BIDER_PRICE_UNE) AS TOTAL_PRICE,\n" +
-                "\t\t\tCOUNT(b.ID) AS bidOkCount\n" +
+                "\t\t\tB.BIDER_PRICE_UNE,\n" +
+                "\t\t\tb.ID as bidId,\n" +
+                "\t\t\tB.IS_BID_SUCCESS,\n" +
+                "\t\t\tB.IS_ABANDON ,\n" +
+                "\t\t\tB.IS_WITHDRAWBID ,\n" +
+                "\t\t\tP.PROJECT_STATUS ,\n" +
+                "\t\t\tP.CREATE_TIME,\n" +
+                "\t\t\tB.BIDER_ID\n" +
                 "\t\tFROM\n" +
                 "\t\t\tBID B\n" +
                 "\t\tLEFT JOIN PROJ_INTER_PROJECT P ON P.ID = B.PROJECT_ID\n" +
-                "\t\tAND p.COMPANY_ID = b.COMPANY_ID\n" +
-                "\t\tWHERE\n" +
-                "\t\t\tB.IS_BID_SUCCESS = 1\n" +
-                "\t\tAND B.IS_ABANDON = 0\n" +
-                "\t\tAND B.IS_WITHDRAWBID = 0\n" +
-                "\t\tAND P.PROJECT_STATUS IN (9, 11, 12)\n" +
-                "\t\tGROUP BY\n" +
-                "\t\t\tB.BIDER_ID\n" +
-                "\t) t\n" +
-                "LEFT JOIN (\n" +
-                "\tSELECT\n" +
-                "\t\tB.BIDER_ID AS pid,\n" +
-                "\t\tCOUNT(p.id) AS bidCount,\n" +
-                "\t\tP.CREATE_TIME\n" +
-                "\tFROM\n" +
-                "\t\tBID B\n" +
-                "\tLEFT JOIN PROJ_INTER_PROJECT P ON P.ID = B.PROJECT_ID\n" +
-                "\tAND p.COMPANY_ID = b.COMPANY_ID\n" +
-                "\tGROUP BY\n" +
-                "\t\tB.BIDER_ID\n" +
-                ") t2 ON t.SUPPLIER_ID = t2.pid\n" +
-                "where t.SUPPLIER_ID IN (\n" +
-                "\tSELECT\n" +
-                "\t\tsupplier_id\n" +
-                "\tFROM\n" +
-                "\t\tbsm_company_supplier_apply\n" +
-                "\tWHERE\n" +
-                "\t\tsupplier_status != 4\n" +
-                ")\n" +
-                "GROUP BY\n" +
-                "\tt.SUPPLIER_ID    ORDER BY t2.CREATE_TIME DESC  " +
+                "\t\tAND p.COMPANY_ID = b.COMPANY_ID " +
                 "\t LIMIT ?,? \n";
         ArrayList<Object> params = new ArrayList<>();
 //        params.add(lastSyncTime);
@@ -219,12 +97,15 @@ public class SyncSupplierBidingStatJobHandler extends SyncJobHandler /*implement
 
 
 
-/*    @Override
+
+
+ /*   @Override
     public void afterPropertiesSet() throws Exception {
         execute();
     }*/
 
-    /*protected void sync(DataSource dataSource, String countSql, String querySql, List<Object> params) {
+    @Override
+    protected void sync(DataSource dataSource, String countSql, String querySql, List<Object> params) {
         long count = DBUtil.count(dataSource, countSql, params);
         logger.debug("执行countSql : {}, params : {}，共{}条", countSql, params, count);
         if (count > 0) {
@@ -232,13 +113,65 @@ public class SyncSupplierBidingStatJobHandler extends SyncJobHandler /*implement
                 // 添加分页查询参数
                 List<Object> paramsToUse = appendToParams(params, i);
                 List<Map<String, Object>> mapList = DBUtil.query(dataSource, querySql, paramsToUse);
-
+                //判断当前供应商是否盘内供应商还是盘外供应商
+                appendSupplierType(mapList);
                 logger.debug("执行querySql : {}, paramsToUse : {}，共{}条", querySql, paramsToUse, mapList.size());
                 // 生成insert sql, 参数
                 if (!CollectionUtils.isEmpty(mapList)) {
-                    System.out.println(mapList);
+                    StringBuilder sqlBuilder = sqlBuilder(mapList);
+                    List<Object> insertParams = buildParams(mapList, sqlBuilder);
+                    DBUtil.execute(reportDataSource, sqlBuilder.toString(), insertParams);
                 }
             }
         }
-    }*/
+    }
+
+    private void appendSupplierType(List<Map<String, Object>> mapList) {
+        Set<Pair> pairs = new HashSet<>();
+        for (Map<String, Object> map : mapList) {
+            pairs.add(new Pair(((long) map.get(COMPANY_ID)), ((long) map.get(SUPPLIER_ID))));
+        }
+
+        StringBuffer querySupplierStatusSql = new StringBuffer("SELECT company_id,supplier_id, supplier_status FROM bsm_company_supplier_apply WHERE ");
+        int count = 0;
+        for (Pair pair : pairs) {
+            if (count > 0) {
+                querySupplierStatusSql.append(" OR ");
+            }
+            querySupplierStatusSql.append(" (company_id=")
+                    .append(pair.companyId)
+                    .append(" AND supplier_id=")
+                    .append(pair.supplierId)
+                    .append(") ");
+            count++;
+        }
+
+        // 查询对应供应商的类型
+        Map<String, String> supplierTypeMap = DBUtil.query(ycDataSource, querySupplierStatusSql.toString(), null, new DBUtil.ResultSetCallback<Map<String, String>>() {
+            @Override
+            public Map<String, String> execute(ResultSet resultSet) throws SQLException {
+                Map<String, String> map = new HashMap<>();
+                while (resultSet.next()) {
+                    long companyId = resultSet.getLong("company_id");
+                    long supplierId = resultSet.getLong("supplier_id");
+                    String supplierStatus = resultSet.getString("supplier_status");
+                    String key = companyId + "_" + supplierId;
+                    map.put(key, supplierStatus);
+                }
+                return map;
+            }
+        });
+
+        for (Map<String, Object> map : mapList) {
+            String key = map.get(COMPANY_ID) + "_" + map.get(SUPPLIER_ID);
+            String supplierStatus = supplierTypeMap.get(key);
+            //saas定义的主要状态不是4都是盘外供应商
+            if("4".equals(supplierStatus)){
+                map.put(SUPPLIER_TYPE, 0);
+            }else{
+                map.put(SUPPLIER_TYPE, 1);
+            }
+
+        }
+    }
 }
