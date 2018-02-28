ae846aaa7fcc92571ac1de1302086235c6a9e2be
zhihuizhou <zhihuizhou@ebnew.com>
2018-02-28 14:15:16 +0800

    企业空间产品定时同步任务

---
+++ bidlink-job-business/src/main/java/cn/bidlink/job/business/handler/SyncEnterpriseSpaceProductDataJobHandler.java
@@ -0,0 +1,253 @@
+package cn.bidlink.job.business.handler;
+
+import cn.bidlink.job.common.es.ElasticClient;
+import cn.bidlink.job.common.utils.DBUtil;
+import cn.bidlink.job.common.utils.ElasticClientUtil;
+import cn.bidlink.job.common.utils.SyncTimeUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.ValueFilter;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.JobHander;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.*;
+
+/**
+ * @author <a href="mailto:zhihuizhou@ebnew.com">zhouzhihui</a>
+ * @version Ver 1.0
+ * @description:
+ * @Date 2018/2/27
+ */
+@JobHander(value = "syncEnterpriseSpaceDataJobHandler")
+@Service
+public class SyncEnterpriseSpaceProductDataJobHandler extends IJobHandler /*implements InitializingBean*/ {
+
+    private Logger logger = LoggerFactory.getLogger(SyncEnterpriseSpaceProductDataJobHandler.class);
+
+    @Autowired
+    @Qualifier("enterpriseSpaceDataSource")
+    private DataSource enterpriseSpaceDataSource;
+
+    @Autowired
+    @Qualifier("centerDataSource")
+    private DataSource centerDataSource;
+
+    @Autowired
+    private ElasticClient elasticClient;
+
+    @Value("${pageSize}")
+    private Integer pageSize;
+
+    private String ID         = "id";
+    private String CORE       = "core";
+    private String COMPANY_ID = "companyId";
+
+    @Override
+    public ReturnT<String> execute(String... strings) throws Exception {
+        SyncTimeUtil.setCurrentDate();
+        logger.info("同步企业空间产品开始");
+        syncEnterpriseSpaceData();
+        logger.info("同步企业空间产品结束");
+        return ReturnT.SUCCESS;
+    }
+
+    private void syncEnterpriseSpaceData() {
+        Timestamp lastTime = ElasticClientUtil.getMaxTimestamp(elasticClient,
+                "cluster.index",
+                "cluster.type.enterprise_space_product",
+                null);
+        logger.info("企业空间同步lastTime:" + new DateTime(lastTime).toString("yyyy-MM-dd HH:mm:ss") + "\n" +
+                ", syncTime:" + new DateTime(SyncTimeUtil.getCurrentDate()).toString("yyyy-MM-dd HH:mm:ss"));
+        syncCreateEnterpriseSpaceService(lastTime);
+        syncUpdateEnterpriseSpaceService(lastTime);
+    }
+
+    /**
+     * 同步更新企业空间产品
+     *
+     * @param lastTime
+     */
+    private void syncUpdateEnterpriseSpaceService(Timestamp lastTime) {
+        String countSql = "SELECT\n" +
+                "\tcount(1)\n" +
+                "FROM\n" +
+                "\tspace_product" +
+                "\tWHERE update_time > ? \n";
+        String querySql = "SELECT\n" +
+                "\tid,\n" +
+                "\tCOMPANYID AS companyId,\n" +
+                "\tPICFILE AS picFile,\n" +
+                "\tOTHERNAME AS productName,\n" +
+                "\tCOMPANYNAME AS companyName,\n" +
+                "\tSTATE AS state,\n" +
+                "\tZONESTR AS zoneStr\n" +
+                "FROM\n" +
+                "\tspace_product \n" +
+                "\tWHERE update_time > ? \n" +
+                "\tLIMIT ?,?";
+        ArrayList<Object> params = new ArrayList<>();
+        params.add(lastTime);
+        doSyncEnterpriseSpaceService(countSql, querySql, params);
+    }
+
+    /**
+     * 同步插入企业空间产品
+     *
+     * @param lastTime
+     */
+    private void syncCreateEnterpriseSpaceService(Date lastTime) {
+        String countSql = "SELECT\n" +
+                "\tcount(1)\n" +
+                "FROM\n" +
+                "\tspace_product" +
+                "\tWHERE create_time > ? \n";
+        String querySql = "SELECT\n" +
+                "\tid,\n" +
+                "\tCOMPANYID AS companyId,\n" +
+                "\tPICFILE AS picFile,\n" +
+                "\tOTHERNAME AS productName,\n" +
+                "\tCOMPANYNAME AS companyName,\n" +
+                "\tSTATE AS state,\n" +
+                "\tZONESTR AS zoneStr\n" +
+                "FROM\n" +
+                "\tspace_product \n" +
+                "\tWHERE create_time > ? \n" +
+                "\tLIMIT ?,?";
+        ArrayList<Object> params = new ArrayList<>();
+        params.add(lastTime);
+        doSyncEnterpriseSpaceService(countSql, querySql, params);
+    }
+
+    private void doSyncEnterpriseSpaceService(String countSql, String querySql, ArrayList<Object> params) {
+        long count = DBUtil.count(enterpriseSpaceDataSource, countSql, params);
+        logger.debug("执行countSql:{}, params:{} , 共{}条", countSql, params, count);
+        for (long i = 0; i < count; i += pageSize) {
+            List<Object> paramsToUse = appendToParams(params, i);
+            List<Map<String, Object>> products = DBUtil.query(enterpriseSpaceDataSource, querySql, paramsToUse);
+
+            // 添加供应商状态
+            appendSupplierStatus(products);
+
+            // 添加同步时间和字段类型转换
+            for (Map<String, Object> product : products) {
+                refresh(product);
+            }
+            // 插入es中
+            batchInsert(products);
+        }
+    }
+
+    /**
+     * 添加供应商状态 core: 0表示非核心供    1:核心供
+     *
+     * @param mapList
+     */
+    private void appendSupplierStatus(List<Map<String, Object>> mapList) {
+        HashSet<Long> companyIds = new HashSet<>();
+        for (Map<String, Object> map : mapList) {
+            companyIds.add(((Long) map.get(COMPANY_ID)));
+        }
+
+        String querySqlTemplate = "SELECT\n" +
+                "\tCOMP_ID AS companyId,\n" +
+                "\tCREDIT_MEDAL_STATUS AS core \n" +
+                "FROM\n" +
+                "\t`t_uic_company_status` \n" +
+                "WHERE\n" +
+                "\tCOMP_ID IN (%s)";
+        String querySql = String.format(querySqlTemplate, StringUtils.collectionToCommaDelimitedString(companyIds));
+        Map<Long, Integer> coreMap = DBUtil.query(centerDataSource, querySql, null, new DBUtil.ResultSetCallback<Map<Long, Integer>>() {
+            @Override
+            public Map<Long, Integer> execute(ResultSet resultSet) throws SQLException {
+                HashMap<Long, Integer> map = new HashMap<>();
+                while (resultSet.next()) {
+                    map.put(resultSet.getLong(COMPANY_ID), resultSet.getInt(CORE));
+                }
+                return map;
+            }
+        });
+
+        for (Map<String, Object> map : mapList) {
+            map.put(CORE, coreMap.get(map.get(COMPANY_ID)));
+        }
+    }
+
+    /**
+     * 添加分页查询
+     *
+     * @param params
+     * @param i
+     * @return
+     */
+    private List<Object> appendToParams(ArrayList<Object> params, long i) {
+        ArrayList<Object> paramsToUse = new ArrayList<>(params);
+        paramsToUse.add(i);
+        paramsToUse.add(pageSize);
+        return paramsToUse;
+    }
+
+    /**
+     * 添加同步时间字段
+     *
+     * @param product
+     */
+    private void refresh(Map<String, Object> product) {
+        product.put(ID, String.valueOf(product.get(ID)));
+        product.put(COMPANY_ID, String.valueOf(product.get(COMPANY_ID)));
+        product.put(SyncTimeUtil.SYNC_TIME, SyncTimeUtil.getCurrentDate());
+    }
+
+    /**
+     * 批量插入es
+     *
+     * @param products
+     */
+    private void batchInsert(List<Map<String, Object>> products) {
+        if (!CollectionUtils.isEmpty(products)) {
+            BulkRequestBuilder bulkRequest = elasticClient.getTransportClient().prepareBulk();
+            for (Map<String, Object> product : products) {
+                bulkRequest.add(elasticClient.getTransportClient()
+                        .prepareIndex(elasticClient.getProperties().getProperty("cluster.index"),
+                                elasticClient.getProperties().getProperty("cluster.type.enterprise_space_product"),
+                                String.valueOf(product.get(ID)))
+                        .setSource(JSON.toJSONString(product, new ValueFilter() {
+                            @Override
+                            public Object process(Object object, String propertyName, Object propertyValue) {
+                                if (propertyValue instanceof java.util.Date) {
+                                    // 是date类型按指定日期格式转换
+                                    return new DateTime(propertyValue).toString(SyncTimeUtil.DATE_TIME_PATTERN);
+                                } else {
+
+                                    return propertyValue;
+                                }
+                            }
+                        })));
+            }
+            BulkResponse response = bulkRequest.execute().actionGet();
+            // 是否失败
+            if (response.hasFailures()) {
+                logger.error(response.buildFailureMessage());
+            }
+        }
+    }
+
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        execute();
+//    }
+}
