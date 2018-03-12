ca61280dc8b39fd502a5d531777503c5133315f5
zhihuizhou <zhihuizhou@ebnew.com>
2018-03-12 17:16:33 +0800

    供应商产品定时任务添加供应商logo字段

--- bidlink-job-business/src/main/java/cn/bidlink/job/business/handler/SyncSupplierDataJobHandler.java
+++ bidlink-job-business/src/main/java/cn/bidlink/job/business/handler/SyncSupplierDataJobHandler.java
@@ -19,7 +19,6 @@ import org.elasticsearch.search.SearchHits;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -49,7 +48,7 @@ import static cn.bidlink.job.common.utils.DBUtil.query;
  */
 @JobHander(value = "syncSupplierDataJobHandler")
 @Service
-public class SyncSupplierDataJobHandler extends JobHandler implements InitializingBean {
+public class SyncSupplierDataJobHandler extends JobHandler /*implements InitializingBean*/ {
     private Logger logger = LoggerFactory.getLogger(SyncSupplierDataJobHandler.class);
 
     @Autowired
@@ -123,11 +122,11 @@ public class SyncSupplierDataJobHandler extends JobHandler implements Initializi
 
     private Map<String, String> industryCodeMap = new HashMap<>();
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        pageSize = 1000;
-        industryCodeMap = initIndustryCodeMap();
-    }
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        pageSize = 1000;
+//        industryCodeMap = initIndustryCodeMap();
+//    }
 
     private Map<String, String> initIndustryCodeMap() {
         String queryIndustrySql = "SELECT code, name_cn, name_en, type from t_reg_code_trade_class";
