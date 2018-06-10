5a7a7cbbbec4b5f75555c8ae88e555379cb8e6dc
zhihuizhou <zhihuizhou@ebnew.com>
2018-06-10 11:21:12 +0800

    完善代码

--- bidlink-job-business/src/main/java/cn/bidlink/job/business/handler/SyncDealProjectDataJobHandler.java
+++ bidlink-job-business/src/main/java/cn/bidlink/job/business/handler/SyncDealProjectDataJobHandler.java
@@ -13,7 +13,6 @@ import org.elasticsearch.action.bulk.BulkResponse;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -34,7 +33,7 @@ import java.util.Map;
  */
 @Service
 @JobHander("syncDealProjectDataJobHandler")
-public class SyncDealProjectDataJobHandler extends JobHandler implements InitializingBean {
+public class SyncDealProjectDataJobHandler extends JobHandler /*implements InitializingBean*/ {
 
     private Logger logger = LoggerFactory.getLogger(SyncDealProjectDataJobHandler.class);
     @Autowired
@@ -187,8 +186,8 @@ public class SyncDealProjectDataJobHandler extends JobHandler implements Initial
         }
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        execute();
-    }
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        execute();
+//    }
 }
