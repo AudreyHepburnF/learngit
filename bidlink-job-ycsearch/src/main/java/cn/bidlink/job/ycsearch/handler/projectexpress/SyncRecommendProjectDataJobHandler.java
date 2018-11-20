254733b70ce14cdb6127e3db2d3174f6e8e4d629
zhihuizhou <zhihuizhou@ebnew.com>
2018-11-20 11:04:23 +0800

    代码优化

--- bidlink-job-ycsearch/src/main/java/cn/bidlink/job/ycsearch/handler/projectexpress/SyncRecommendProjectDataJobHandler.java
+++ bidlink-job-ycsearch/src/main/java/cn/bidlink/job/ycsearch/handler/projectexpress/SyncRecommendProjectDataJobHandler.java
@@ -23,7 +23,6 @@ import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -48,7 +47,7 @@ import static cn.bidlink.job.common.utils.SyncTimeUtil.getZeroTimeLongValue;
  */
 @Service
 @JobHander(value = "syncRecommendProjectDataJobHandler")
-public class SyncRecommendProjectDataJobHandler extends JobHandler implements InitializingBean {
+public class SyncRecommendProjectDataJobHandler extends JobHandler /*implements InitializingBean*/ {
 
     private Logger logger = LoggerFactory.getLogger(SyncRecommendProjectDataJobHandler.class);
 
@@ -531,8 +530,8 @@ public class SyncRecommendProjectDataJobHandler extends JobHandler implements In
         return false;
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        execute();
-    }
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        execute();
+//    }
 }
