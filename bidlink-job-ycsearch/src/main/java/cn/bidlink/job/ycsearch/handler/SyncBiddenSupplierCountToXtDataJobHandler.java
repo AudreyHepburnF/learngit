60224067ff67b3271dcb87e283c257deefeaabe0
yangyanglei <yangyanglei@ebnew.com>
2019-04-08 16:40:12 +0800

    统一代码都使用update

--- bidlink-job-ycsearch/src/main/java/cn/bidlink/job/ycsearch/handler/SyncBiddenSupplierCountToXtDataJobHandler.java
+++ bidlink-job-ycsearch/src/main/java/cn/bidlink/job/ycsearch/handler/SyncBiddenSupplierCountToXtDataJobHandler.java
@@ -86,6 +86,7 @@ public class SyncBiddenSupplierCountToXtDataJobHandler extends JobHandler /*impl
                 .setTypes(properties.getProperty("cluster.type.supplier_opportunity"))
                 .setQuery(queryBuilder)
                 .setScroll(new TimeValue(60000))
+                .setFetchSource(new String[]{ID,PROJECT_ID, PURCHASE_ID,PROJECT_TYPE}, null)
                 .setSize(batchInsert)
                 .get();
         int i = 0;
