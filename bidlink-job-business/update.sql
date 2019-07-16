bd57dcf59a7cc105c7e750b376e8cc9adf2d606b
simon <hrfhwy@163.com>
2019-07-16 11:33:23 +0800

    修改需要更新的sql

--- bidlink-job-business/update.sql
+++ bidlink-job-business/update.sql
@@ -1,10 +1,2 @@
 
 # 需要更新的sql
-
-# 添加字段
-supplier_opportunity/supplier_opportunity/_mapping PUT
-{"properties":{"ifApprove":{"type":"integer"}}}
-
-# 添加字段
-recruit/recruit/_mapping PUT
-{"properties":{"ifApprove":{"type":"integer"}}}
